bec0a2951b7dd69f001a70c56a983815077b1b7a
FraSma <francemazzi@gmail.com>
2023-03-15 17:23:55 +0100

    feat: init db

--- prisma/migrations/20230312101321_db_admin/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "admin" BOOLEAN,
-ADD COLUMN     "cognome" TEXT,
-ADD COLUMN     "nome" TEXT;
--- prisma/migrations/20230315143424_adminpermissions/migration.sql
+++ prisma/migrations/20230315143424_adminpermissions/migration.sql
@@ -1,4 +1,23 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "nome" TEXT,
+    "cognome" TEXT,
+    "admin" BOOLEAN,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Password" (
+    "hash" TEXT NOT NULL,
+    "userId" TEXT NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "Import" (
     "id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -42,8 +61,29 @@ CREATE TABLE "ImportsOnCategory" (
     CONSTRAINT "ImportsOnCategory_pkey" PRIMARY KEY ("importId","categoryId")
 );
 
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "CategoryImport_name_key" ON "CategoryImport"("name");
+
+-- AddForeignKey
+ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ImportsOnUsers" ADD CONSTRAINT "ImportsOnUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -56,3 +96,6 @@ ALTER TABLE "ImportsOnCategory" ADD CONSTRAINT "ImportsOnCategory_importId_fkey"
 
 -- AddForeignKey
 ALTER TABLE "ImportsOnCategory" ADD CONSTRAINT "ImportsOnCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "CategoryImport"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
